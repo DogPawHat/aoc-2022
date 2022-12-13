708bee0e422044fcd15551d866232c88fd69949e
Ciarán Curley <ciaran.curley.11.11@gmail.com>
2022-12-13 22:44:05 +0000

    feat: forest normalized

--- src/bin/day8.rs
+++ src/bin/day8.rs
@@ -1,6 +1,6 @@
-use std::fs;
+use std::{collections::HashMap, fs, str::FromStr};
 
-use anyhow::{anyhow, bail};
+use anyhow::{bail};
 use lazy_static::lazy_static;
 
 pub type Error = anyhow::Error;
@@ -13,14 +13,61 @@ lazy_static! {
         fs::read_to_string(INPUT_PATH).expect("Day 7 - Inputs: Can't parse stacks");
 }
 
-fn part1(file: &str) -> Result<u32> {
+#[derive(Debug)]
+struct ElfTree {
+    height: u32,
+}
+
+#[derive(Debug, PartialEq, Eq, Hash)]
+struct ElfTreePos {
+    x: usize,
+    y: usize,
+}
+
+#[derive(Debug)]
+struct ElfForest(HashMap<ElfTreePos, ElfTree>);
+
+impl FromStr for ElfForest {
+    type Err = anyhow::Error;
+
+    fn from_str(s: &str) -> Result<Self> {
+        let mut forest_map = HashMap::new();
+
+        let mut current_y = 0;
+        for line in s.lines() {
+            let mut current_x = 0;
+            for tree_char in line.chars() {
+                match u32::from_str(&String::from(tree_char)) {
+                    Ok(height) => {
+                        forest_map.insert(
+                            ElfTreePos {
+                                x: current_x,
+                                y: current_y,
+                            },
+                            ElfTree { height },
+                        );
+                    }
+                    Err(err) => bail!("Issue with: {:?}", err),
+                }
+                current_x = current_x + 1;
+            }
+            current_y = current_y + 1;
+        }
+
+        Ok(Self(forest_map))
+    }
+}
+
+fn part1(_forest: &ElfForest) -> Result<u32> {
     todo!()
 }
 
 fn main() -> Result<()> {
+    let forest = ElfForest::from_str(&INPUT_FILE)?;
+
     println!(
         "Part 1 - Trees visible from outside grid: <{}>",
-        part1(&INPUT_FILE)?
+        part1(&forest)?
     );
     Ok(())
 }
