db21e8b92221d328a0c318e2001c5ae9c81a32a1
Ciarán Curley <ciaran.curley.11.11@gmail.com>
2022-12-13 22:16:47 +0000

    fix: clean init state

--- src/bin/day8.rs
+++ src/bin/day8.rs
@@ -7,17 +7,19 @@ pub type Error = anyhow::Error;
 pub type Result<T> = anyhow::Result<T>;
 
 const INPUT_PATH: &str = "inputs/day8.txt";
-const BIG_DIRECTORY_SIZE: u32 = 100000;
 
 lazy_static! {
     static ref INPUT_FILE: String =
         fs::read_to_string(INPUT_PATH).expect("Day 7 - Inputs: Can't parse stacks");
 }
 
+fn part1(file: &str) -> Result<u32> {
+    todo!()
+}
+
 fn main() -> Result<()> {
     println!(
-        "Part 1 - Total size of directories with size > {}: <{}>",
-        BIG_DIRECTORY_SIZE,
+        "Part 1 - Trees visible from outside grid: <{}>",
         part1(&INPUT_FILE)?
     );
     Ok(())
