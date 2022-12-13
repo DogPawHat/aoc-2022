9b6e6b85c3b446b92d9120cd87b0eb227395fd66
Ciarán Curley <ciaran.curley.11.11@gmail.com>
2022-12-13 23:33:45 +0000

    fix: stuff

--- src/bin/day8.rs
+++ src/bin/day8.rs
@@ -1,6 +1,6 @@
-use std::{collections::HashMap, fs, str::FromStr};
+use std::{fs, str::FromStr};
 
-use anyhow::{bail};
+use anyhow::bail;
 use lazy_static::lazy_static;
 
 pub type Error = anyhow::Error;
@@ -18,52 +18,80 @@ struct ElfTree {
     height: u32,
 }
 
-#[derive(Debug, PartialEq, Eq, Hash)]
-struct ElfTreePos {
+#[derive(Debug)]
+struct ElfForest(Vec<Vec<ElfTree>>);
+
+struct UpForestIter{
+    forest: Vec<Vec<ElfTree>>,
     x: usize,
     y: usize,
 }
 
-#[derive(Debug)]
-struct ElfForest(HashMap<ElfTreePos, ElfTree>);
+impl UpForestIter {
+    pub fn iter(&self) -> U
+}
+
+impl Iterator for UpForestIter {
+    type Item = &ElfTree;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let next_item = self.forest.get(self.x)?.get(self.y)?;
+        self.y = self.y - 1;
+        Some(next_item)
+    }
+    
+}
 
 impl FromStr for ElfForest {
     type Err = anyhow::Error;
 
     fn from_str(s: &str) -> Result<Self> {
-        let mut forest_map = HashMap::new();
+        let forest_map_res: Result<Vec<Vec<_>>> = s
+            .lines()
+            .map(|line| {
+                let column = line
+                    .chars()
+                    .map(|s| {
+                        let height = String::from(s).parse()?;
+                        Ok(ElfTree { height })
+                    })
+                    .collect();
+                column
+            })
+            .collect();
 
-        let mut current_y = 0;
-        for line in s.lines() {
-            let mut current_x = 0;
-            for tree_char in line.chars() {
-                match u32::from_str(&String::from(tree_char)) {
-                    Ok(height) => {
-                        forest_map.insert(
-                            ElfTreePos {
-                                x: current_x,
-                                y: current_y,
-                            },
-                            ElfTree { height },
-                        );
-                    }
-                    Err(err) => bail!("Issue with: {:?}", err),
-                }
-                current_x = current_x + 1;
-            }
-            current_y = current_y + 1;
-        }
+        let forest_map = forest_map_res?;
+
+        assert!(forest_map.first().unwrap().len() == forest_map.last().unwrap().len());
+        assert!(forest_map[0].len() == forest_map[1].len());
+        assert!(forest_map[1].len() == forest_map.last().unwrap().len());
 
         Ok(Self(forest_map))
     }
 }
+
+impl ElfForest {
+    fn get_max_x(&self) -> usize {
+        self.0[0].len()
+    }
+
+    fn get_max_y(&self) -> usize {
+        self.0.len()
+    }
+
+    fn is_tree_visible(&self, x: usize, y: usize) -> bool {
+        let tree = self.0.get(x).unwrap().get(y).unwrap();
+
+    }
+}
+
 
 fn part1(_forest: &ElfForest) -> Result<u32> {
     todo!()
 }
 
 fn main() -> Result<()> {
-    let forest = ElfForest::from_str(&INPUT_FILE)?;
+    let forest: ElfForest = INPUT_FILE.parse()?;
 
     println!(
         "Part 1 - Trees visible from outside grid: <{}>",
